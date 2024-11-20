355b1265fd18379f74ce2fd3000b1740254ba70a
sn0w12 <lucasskold49@gmail.com>
2024-11-20 01:30:54 +0100

    Fetch data on server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
-"use client";
-
 import { DiscordStatsComponent } from "@/components/discord-stats";
+import { getMessageStats, getVoiceStats, getUserMap } from "@/lib/piego";
 
-export default function Home() {
+export default async function Home() {
+    const [messageStats, voiceStats, userMap] = await Promise.all([
+        getMessageStats(),
+        getVoiceStats(),
+        getUserMap(),
+    ]);
+    console.log(messageStats, voiceStats, userMap);
+
     return <DiscordStatsComponent />;
 }
