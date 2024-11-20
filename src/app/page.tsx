6bc698383b7b609ea904603d9142d0a393ea99ab
sn0w12 <lucasskold49@gmail.com>
2024-11-20 01:37:53 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { DiscordStatsComponent } from "@/components/discord-stats";
 import { getMessageStats, getVoiceStats, getUserMap } from "@/lib/piego";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
     const [messageStats, voiceStats, userMap] = await Promise.all([
         getMessageStats(),
